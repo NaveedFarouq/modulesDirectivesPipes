e0f6415ce24e8e5102a688f73ad30a9b8ce7f607
Naveed <naveedfarouq@hotmail.co.uk>
2019-04-14 17:05:23 +0100

    passing arguments to pipes

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return value.split('').reverse().join('');
+    if(args){
+      return value.split(' ').reverse().join(' ');
+    } else {
+      return value.split('').reverse().join('');
+    }
   }
 
 }
