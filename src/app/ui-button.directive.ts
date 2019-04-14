5915d016fa5640c854fe56311a285c9de08a44b2
Naveed <naveedfarouq@hotmail.co.uk>
2019-04-14 15:15:59 +0100

    handling events in directives

--- src/app/ui-button.directive.ts
+++ src/app/ui-button.directive.ts
@@ -1,22 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appUiButton]'
 })
 export class UiButtonDirective {
-
-  constructor( elementRef: ElementRef) { 
-
-  
-    Object.assign(elementRef.nativeElement.style, {
-      backgroundColor: '#ff00a6',
-      padding: '7px 15px',
-      fontSize: '16px',
-      color: '#fff',
-      border: 'none',
+  constructor(private el: ElementRef) {
+    Object.assign(el.nativeElement.style, {
+      backgroundColor: '#ff00a6', 
+      padding: '7px 15px', 
+      fontSize: '16px', 
+      color: '#fff', 
+      border: 'none', 
       borderRadius: '4px'
     })
   }
 
-
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = '#000';
+  }
+ 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = '#ff00a6';
+  }
 }
